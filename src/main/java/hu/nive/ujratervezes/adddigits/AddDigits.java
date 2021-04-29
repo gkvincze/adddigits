38cddec975deb1849b4e9024a5509e9e02ed59a1
Gabor Vincze <gkvincze@gmail.com>
2021-04-29 10:46:44 +0200

    addDigits implemented

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,27 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+    public int  addDigits (String input) {
+        int result = 0;
+        StringBuilder temp = new StringBuilder("0");
+
+        if (input == null || input.equals("")) {
+            return -1;
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                temp.append(ch);
+            } else {
+                result += Integer.parseInt(temp.toString());
+                temp = new StringBuilder("0");
+            }
+
+        }
+
+        return result + Integer.parseInt(temp.toString());
+    }
 }
+
